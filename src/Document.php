6b30362118338e89c26ea9a12a18183d2e5f3939
Marc Neudert <marc.neudert@gmail.com>
2014-06-16 20:31:12 +0200

    removes Document->write() helper method

--- src/Document.php
+++ src/Document.php
@@ -7,18 +7,5 @@ use JUnitScribe\Writer\File as FileWriter;
 
 class Document extends JUnitTestsuite
 {
-    /**
-     * Writes the current document to the given file.
-     *
-     * @param   string  $filename
-     * @return  bool
-     */
-    public function write($filename)
-    {
-        $writer = new FileWriter();
-
-        $writer->setDocument($this);
-
-        return $writer->write($filename);
-    }
+    /** intentionally left blank */
 }
