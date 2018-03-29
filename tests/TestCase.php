cd7e72cfb8c732c611ab6639f76e214db96336cf
Marc Neudert <marc.neudert@gmail.com>
2018-03-29 09:31:53 +0200

    bumps phpunit version

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace JUnitScribeTest;
 
-class TestCase extends \PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected function getFixtureDocument()
     {
